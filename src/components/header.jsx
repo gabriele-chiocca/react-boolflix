18afff9af1ae3a692ff0b3a045313a524e58c9bf
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-02-24 17:08:08 +0100

    default layout adjust with the handlesearchfunction and header with props

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,4 @@
-import { NavLink } from 'react-router-dom';
-
-export default function Header() {
+export default function Header({ query, setQuery, handleSearch }) {
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -20,9 +18,11 @@ export default function Header() {
         </button>
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <form className="form-inline displaynavbar ms-auto mx-4 my-2 my-lg-0">
+          <form className="form-inline displaynavbar ms-auto mx-4 my-2 my-lg-0 d-flex gap-2">
             <input
               className="form-control mx-sm-2"
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
               type="search"
               placeholder="Search"
               aria-label="Search"
@@ -36,50 +36,6 @@ export default function Header() {
           </form>
         </div>
       </nav>
-
-      {/* <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <NavLink className="navbar-brand ms-3" to="/">
-          Store Name
-        </NavLink>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <NavLink className="nav-link active" to="/">
-                Home
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link active" to="/Products">
-                Products
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link active" to="/About-us">
-                About Us
-              </NavLink>
-            </li>
-          </ul>
-        </div>
-      </nav> */}
     </>
   );
 }
-
-/*
-      <header className="bg-light mb-3 p-3 d-flex">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/products">Products</NavLink>
-        <NavLink to="/About-us">About Us</NavLink>
-      </header>*/
--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,13 +1,44 @@
 import Header from '../components/header';
 import { Outlet } from 'react-router-dom';
+import axios from 'axios';
+import { useState } from 'react';
 
 export default function DefaultLayout() {
+  const [query, setQuery] = useState('');
+  const [movies, setMovies] = useState([]);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+
+    if (!query.trim()) return;
+
+    axios
+      .get('https://api.themoviedb.org/3/search/movie', {
+        params: {
+          api_key: 'cd2a620980eb7bec8a562f62a1cf9b08',
+          query: query,
+          language: 'it-IT',
+        },
+      })
+      .then((res) => {
+        setMovies(res.data.results);
+        console.log(res.data.results);
+      })
+      .catch((error) => {
+        console.error('Errore ' + error);
+      });
+  };
+
   return (
     <>
-      <Header></Header>
+      <Header
+        query={query}
+        setQuery={setQuery}
+        handleSearch={handleSearch}
+      ></Header>
       <main>
         <div className="mt-3 container">
-          <Outlet></Outlet>
+          <Outlet context={movies}></Outlet>
         </div>
       </main>
     </>
