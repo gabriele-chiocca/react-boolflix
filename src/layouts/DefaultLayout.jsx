6863d5ea98998324a9326a919631c0e8c79074ce
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-02-24 17:29:53 +0100

    add series axios call to the handleSearch

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -6,6 +6,7 @@ import { useState } from 'react';
 export default function DefaultLayout() {
   const [query, setQuery] = useState('');
   const [movies, setMovies] = useState([]);
+  const [series, setSeries] = useState([]);
 
   const handleSearch = (e) => {
     e.preventDefault();
@@ -27,6 +28,22 @@ export default function DefaultLayout() {
       .catch((error) => {
         console.error('Errore ' + error);
       });
+
+    axios
+      .get('https://api.themoviedb.org/3/search/tv', {
+        params: {
+          api_key: 'cd2a620980eb7bec8a562f62a1cf9b08',
+          query: query,
+          language: 'it-IT',
+        },
+      })
+      .then((res) => {
+        setSeries(res.data.results);
+        console.log(res.data.results);
+      })
+      .catch((error) => {
+        console.error('Errore ' + error);
+      });
   };
 
   return (
@@ -38,7 +55,7 @@ export default function DefaultLayout() {
       ></Header>
       <main>
         <div className="mt-3 container">
-          <Outlet context={{ movies }}></Outlet>
+          <Outlet context={{ movies, series }}></Outlet>
         </div>
       </main>
     </>
