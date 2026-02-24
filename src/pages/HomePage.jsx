755370823f4463f11345dd3871211e968ce1a247
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-02-24 16:51:26 +0100

    axios call try

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,32 @@
-import { NavLink } from 'react-router-dom';
-import Header from '../components/header';
+import axios from 'axios';
+import { useState } from 'react';
 
 export default function HomePage() {
+  const [query, setQuery] = useState('');
+  const [movies, setMovies] = useState([]);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+
+    if (!query.trim()) return;
+
+    axios
+      .get('https://api.themoviedb.org/3/search/movie', {
+        params: {
+          api_key: 'cd2a620980eb7bec8a562f62a1cf9b08',
+          query: query,
+          language: 'it-IT',
+        },
+      })
+      .then((res) => {
+        setMovies(res.data.results);
+        console.log(res.data.results);
+      })
+      .catch((error) => {
+        console.error('Errore ' + error);
+      });
+  };
+
   return (
     <>
       <h1 className="text-center mt-5">Homepage</h1>;
