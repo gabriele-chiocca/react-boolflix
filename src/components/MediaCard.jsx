2ba6164f137104d6eaa20228b8dcac8652267a47
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-02-25 15:01:20 +0100

    added original title, overview and stars

--- src/components/MediaCard.jsx
+++ src/components/MediaCard.jsx
@@ -2,10 +2,15 @@ import { getFlagUrl } from '../function/flag';
 
 export default function MediaCard({ item, isTv }) {
   const title = isTv ? item.name : item.title;
+  const titleOriginal = isTv ? item.original_name : item.original_title;
 
   const posterUrl = item.poster_path
     ? `https://image.tmdb.org/t/p/w342${item.poster_path}`
     : null;
+
+  const stars = Math.max(1, Math.ceil(item.vote_average / 2));
+  const fullStars = '★'.repeat(stars);
+  const emptyStars = '☆'.repeat(5 - stars);
 
   return (
     <div className="card media-card cardwith">
@@ -18,7 +23,30 @@ export default function MediaCard({ item, isTv }) {
       )}
 
       <div className="media-overlay">
-        <h5>{title}</h5>
+        <div>
+          <h5>Titolo: {title}</h5>
+        </div>
+        <div>
+          <span>
+            <strong>Titolo originale:</strong> {titleOriginal}
+          </span>
+        </div>
+        <div>
+          <span>
+            Voto : {fullStars}
+            {emptyStars}
+          </span>
+        </div>
+        <div>
+          {item.overview ? (
+            <span>
+              <strong>Overview: </strong>
+              {item.overview}
+            </span>
+          ) : (
+            <span>Non è disponibile la descrizione</span>
+          )}
+        </div>
       </div>
     </div>
   );
